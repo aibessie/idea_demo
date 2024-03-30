6dd5fb1529f5680e4c8ddc404a5746783affde76
aibessie <243078372@qq.com>
2024-03-30 15:27:50 +0800

    refactor Caluctor to use reflect

--- src/main/java/Computefactory.java
+++ src/main/java/Computefactory.java
@@ -3,14 +3,7 @@ public class Computefactory {
     private static Add add = new Add();
     private static Sub sub = new Sub();
 
-    public static Computable getCompute(char symbol){
-        switch (symbol){
-            case '+':
-                return add;
-            case '-':
-                return sub;
-            default:
-                throw new IllegalArgumentException();
-        }
+    public static Computable getCompute(String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        return (Computable) Class.forName(symbol).newInstance();
     }
 }
